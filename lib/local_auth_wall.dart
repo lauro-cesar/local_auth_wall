af81b80d2b8288048f879e606abd74c69cf897d7
Lauro C Oliveira <lauro@hostcert.com.br>
2022-12-08 19:37:35 -0300

    Fixed late import

--- lib/local_auth_wall.dart
+++ lib/local_auth_wall.dart
@@ -1,9 +1,9 @@
 library local_auth_wall;
 import 'package:flutter/material.dart';
-import 'package:local_auth_wall/src/auth_wall_overlay_controller.dart';
 import 'package:provider/provider.dart';
-import '/src/auth_wall_boot_controller.dart';
+
 import '/src/auth_wall_notifier.dart';
+import '/src/auth_wall_overlay_controller.dart';
 
 
 ///
