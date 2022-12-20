187ab7d5278804a3db7c6745ea65bb1d35c37f07
Lauro C Oliveira <lauro@hostcert.com.br>
2022-12-20 06:14:59 -0300

    Fixed non Expanded Widget

--- lib/src/auth_wall_overlay_controller.dart
+++ lib/src/auth_wall_overlay_controller.dart
@@ -17,53 +17,53 @@ class AuthWallOverlayController extends StatelessWidget {
         AuthWallBootController(),
 
         ///
-        AnimatedPositioned(
-            right: 0,
-            left: 0,
-            top: (context.watch<AuthWallNotifier>().showLocalAuth)
-                ? 0
-                : (MediaQuery.of(context).size.height),
-            bottom: 0,
-            duration: Duration(milliseconds: 400),
-            curve: Curves.easeInOutQuad,
-
-            ///
-            child: Container(
-              color: Colors.black.withOpacity(0.5),
-              alignment: Alignment.bottomCenter,
-              child: SizedBox(
-                height: MediaQuery.of(context).size.height * 0.3,
-                child: Container(
-                  color: Colors.white,
-                  alignment: Alignment.center,
-                  child: Column(
-                    children: [
-                      ///
-                      SizedBox(
-                        height: 48,
-                        width: 48,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: CircularProgressIndicator(),
-                        ),
-                      ),
-
-                      ///
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: IconButton(
-                            onPressed: () {
-                              context
-                                  .read<AuthWallNotifier>()
-                                  .dismissLocalAuth();
-                            },
-                            icon: Icon(Icons.cancel)),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            )),
+        // AnimatedPositioned(
+        //     right: 0,
+        //     left: 0,
+        //     top: (context.watch<AuthWallNotifier>().showLocalAuth)
+        //         ? 0
+        //         : (MediaQuery.of(context).size.height),
+        //     bottom: 0,
+        //     duration: Duration(milliseconds: 400),
+        //     curve: Curves.easeInOutQuad,
+        //
+        //     ///
+        //     child: Container(
+        //       color: Colors.black.withOpacity(0.5),
+        //       alignment: Alignment.bottomCenter,
+        //       child: SizedBox(
+        //         height: MediaQuery.of(context).size.height * 0.3,
+        //         child: Container(
+        //           color: Colors.white,
+        //           alignment: Alignment.center,
+        //           child: Column(
+        //             children: [
+        //               ///
+        //               SizedBox(
+        //                 height: 48,
+        //                 width: 48,
+        //                 child: Padding(
+        //                   padding: const EdgeInsets.all(8.0),
+        //                   child: CircularProgressIndicator(),
+        //                 ),
+        //               ),
+        //
+        //               ///
+        //               Padding(
+        //                 padding: const EdgeInsets.all(8.0),
+        //                 child: IconButton(
+        //                     onPressed: () {
+        //                       context
+        //                           .read<AuthWallNotifier>()
+        //                           .dismissLocalAuth();
+        //                     },
+        //                     icon: Icon(Icons.cancel)),
+        //               )
+        //             ],
+        //           ),
+        //         ),
+        //       ),
+        //     )),
       ],
     );
   }
